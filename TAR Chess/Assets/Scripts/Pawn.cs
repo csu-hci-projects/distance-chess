1c96444c2e5fd4ffa1ee5a0e819d6d18fb4623c7
Andreas Johnson <ondryice@rams.colostate.edu>
2022-04-06 15:12:32 -0600

    added basic validity check to pawn moves / takes

--- TAR Chess/Assets/Scripts/Pawn.cs
+++ TAR Chess/Assets/Scripts/Pawn.cs
@@ -12,17 +12,16 @@ public class Pawn : MonoBehaviour {
     void Start() {
         board.put("wp", position);
         possibleMoves = new string[4];
-        possibleMoves[0] = pawnForwardMove();
-        possibleMoves[1] = getMoveTile(0,white?2:-2);
-        possibleMoves[2] = pawnTakeMove(true);
-        possibleMoves[3] = pawnTakeMove(false);
+        updatePossibleMoves();
     }
 
     void Update() {
         
     }
 
-    string pawnForwardMove() { return getMoveTile(0,white?1:-1); }
+    string pawnForwardMove(bool fast = false) {
+        return getMoveTile(0,(white? 1:-1)*(fast? 2:1));
+    }
     string pawnTakeMove(bool left) {
         int fileChange = white?1:-1;
         if(left) fileChange *= -1;
@@ -41,4 +40,34 @@ public class Pawn : MonoBehaviour {
 
     int getFile() { return Board.file(position); }
     int getRank() { return Board.rank(position); }
+
+    public void updatePossibleMoves() {
+        possibleMoves[0] = pawnForwardMove();
+        if(position[1] == (white? '2':'7'))
+            possibleMoves[1] = pawnForwardMove(true);
+        else possibleMoves[1] = null;
+        possibleMoves[2] = pawnTakeMove(true);
+        possibleMoves[3] = pawnTakeMove(false);
+        checkMoveSpace();
+    }
+    private void checkMoveSpace() {
+        if(!allowedMove(possibleMoves[0])) possibleMoves[0] = null;
+        if(!allowedMove(possibleMoves[1])) possibleMoves[1] = null;
+        if(!allowedCapture(possibleMoves[2]))
+            possibleMoves[2] = null;
+        if(!allowedCapture(possibleMoves[3]))
+            possibleMoves[3] = null;
+    }
+    private bool allowedMove(string move) { 
+        if(move is null) return false;
+        string piece = board.pieceAt(move);
+        return (piece is null);
+    }
+    private bool allowedCapture(string move) {
+        if(move is null) return false;
+        string piece = board.pieceAt(move);
+        if(!(piece is null) && piece[0] == (white? 'b' : 'w'))
+            return true;
+        else return false;
+    }
 }
