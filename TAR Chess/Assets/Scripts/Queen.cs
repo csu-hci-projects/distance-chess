fbfb171cd1c070561696867e2d8fea5dc74cdab8
Andreas Johnson <ondryice@rams.colostate.edu>
2022-04-19 12:24:39 -0600

    move functionality

--- TAR Chess/Assets/Scripts/Queen.cs
+++ TAR Chess/Assets/Scripts/Queen.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Queen : MonoBehaviour {
     public Board board;
     public bool white, alive;
-    public string position, movePosition=null;
+    public string position, movePosition=null, pin=null;
     public List<string> possibleMoves = new List<string>();
 
     void Start() {
@@ -19,6 +19,26 @@ public class Queen : MonoBehaviour {
     }
 
     public void updatePossibleMoves() {
-        
+        possibleMoves = Utils.getQueenAttacksFrom(board, white, position);
+        List<string> illegalMoves = new List<string>();
+        foreach(string move in possibleMoves) {
+            string piece = board.pieceAt(move);
+            if(Utils.validPiece(piece) && Utils.pieceColor(piece) == (white? 'w':'b'))
+                illegalMoves.Add(move);
+        }
+        if(Utils.validPosition(pin)) { // if this piece is pinned
+            List<string> betweens = Utils.getPositionsBetween(position, pin);
+            foreach(string move in possibleMoves) {
+                if(move.Equals(pin)) // move is legal if it captures the pinning piece
+                    continue;
+                if(betweens.Contains(move)) // move is legal if it is within the pin
+                    continue;
+                illegalMoves.Add(move); // otherwise, the move is illegal
+            }
+        }
+
+        // remove all the illegal moves
+        foreach(string move in illegalMoves)
+            possibleMoves.Remove(move);
     }
 }
