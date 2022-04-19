fe5f362f98d5c3f9be742c774fcd710fcfd8c395
Andreas Johnson <ondryice@rams.colostate.edu>
2022-04-19 12:08:30 -0600

    initial scripting, prefab & scene updates

--- TAR Chess/Assets/Scripts/Queen.cs
+++ TAR Chess/Assets/Scripts/Queen.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Queen : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Queen : MonoBehaviour {
+    public Board board;
+    public bool white, alive;
+    public string position, movePosition=null;
+    public List<string> possibleMoves = new List<string>();
+
+    void Start() {
+        board.put(Utils.piece(white, 'q'), position);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        if(board.needsUpdate(position)) {
+            updatePossibleMoves();
+        }
+    }
+
+    public void updatePossibleMoves() {
         
     }
 }
