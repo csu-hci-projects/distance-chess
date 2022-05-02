62295b0cf4e39a5256efc5be27e2ac1ce0a4f183
Andreas Johnson <ondryice@rams.colostate.edu>
2022-05-02 16:38:24 -0600

    rook moves

--- TAR Chess/Assets/Scripts/Rook.cs
+++ TAR Chess/Assets/Scripts/Rook.cs
@@ -7,4 +7,18 @@ public class Rook : Piece {
     void Update() {
         
     }
+
+    public override bool validMove(string move) => validMove(Utils.file(move), Utils.rank(move));
+    public override bool validMove(int file, int rank) {
+        int cf=this.file(), cr=this.rank();
+        if(cf != file && cr != rank)
+            return false;
+        if(cf == file && cr == rank)
+            return false;
+        for(int dist=1; dist<Mathf.Abs(file-cf); ++dist) {
+            if(game.occupied((cf==file? cf:cf+dist), (cr==rank? cr:cr+dist)))
+                return false;
+        }
+        return base.validMove(file, rank);
+    }
 }
